55ff1c08de4e92353252676dba91d004982381aa
BulutKaradag <40030779+BulutKaradag@users.noreply.github.com>
2024-03-28 15:35:04 +0300

    Update KYCInsurance.sol

--- KYCInsurance.sol
+++ KYCInsurance.sol
@@ -3,32 +3,33 @@ pragma solidity ^0.8.0;
 
 contract EventInfoRegistry {
     struct EventInfo {
+		string insuranceCompany;
         string province;
         string city;
         string plate;
         string eventDate;
         string insuranceBranch;
-        string comment;
+
     }
 
     mapping(uint256 => EventInfo[]) private eventInfoRegistry;
 
     function register(
         uint256 taxNumber,
+		string memory insuranceCompany,
         string memory province,
         string memory city,
         string memory plate,
         string memory eventDate,
-        string memory insuranceBranch,
-        string memory comment
+        string memory insuranceBranch
     ) public {
         eventInfoRegistry[taxNumber].push(EventInfo(
-            province,
+			insuranceCompany,
+			province,
             city,
             plate,
             eventDate,
-            insuranceBranch,
-            comment
+            insuranceBranch
         ));
     }
 
@@ -38,28 +39,28 @@ contract EventInfoRegistry {
     string[] memory,
     string[] memory,
     string[] memory,
-    string[] memory
+	string[] memory
 ) {
     require(eventInfoRegistry[taxNumber].length > 0, "No data found with the given tax number");
 
     //  tüm bilgileri topla
+	string[] memory insuranceCompanies = new string[](eventInfoRegistry[taxNumber].length);
     string[] memory provinces = new string[](eventInfoRegistry[taxNumber].length);
     string[] memory citys = new string[](eventInfoRegistry[taxNumber].length);
     string[] memory plates = new string[](eventInfoRegistry[taxNumber].length);
     string[] memory eventDates = new string[](eventInfoRegistry[taxNumber].length);
     string[] memory insuranceBranchs = new string[](eventInfoRegistry[taxNumber].length);
-    string[] memory comments = new string[](eventInfoRegistry[taxNumber].length);
 
     for (uint256 i = 0; i < eventInfoRegistry[taxNumber].length; i++) {
+        insuranceCompanies[i] = eventInfoRegistry[taxNumber][i].insuranceBranch;
         provinces[i] = eventInfoRegistry[taxNumber][i].province;
         citys[i] = eventInfoRegistry[taxNumber][i].city;
         plates[i] = eventInfoRegistry[taxNumber][i].plate;
         eventDates[i] = eventInfoRegistry[taxNumber][i].eventDate;
         insuranceBranchs[i] = eventInfoRegistry[taxNumber][i].insuranceBranch;
-        comments[i] = eventInfoRegistry[taxNumber][i].comment;
     }
 
-    return (provinces, citys, plates, eventDates, insuranceBranchs, comments);
+    return (insuranceCompanies, provinces, citys, plates, eventDates,insuranceBranchs);
 }
 
 }
